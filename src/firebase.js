d908a8472791019eca70364f1161a526edf646c9
matad9rius <ramonunivigo@gmail.com>
2021-03-27 01:33:01 -0600

    add styled components cart home page and firebase db

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAtwCKy64aTq-y6mKuHnAyFLoqkmRf-ZG8",
+    authDomain: "fir-b2571.firebaseapp.com",
+    projectId: "fir-b2571",
+    storageBucket: "fir-b2571.appspot.com",
+    messagingSenderId: "832136970258",
+    appId: "1:832136970258:web:9b49aa9be0058476d298cb",
+    measurementId: "G-DL9JEE08SF"
+});
+
+const db = firebase.firestore();
+
+export { db }
